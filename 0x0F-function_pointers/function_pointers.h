42e5979d6bb562308147afd5c814377b616e9120
254-elvis <mureithielvis18@gmail.com>
2022-10-13 05:44:17 -0700

    hd

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
-#ifndef MAIN.C
-#define MAIN.C
+#ifndef FUNCTION_POINTERS.H
+#define FUNCTION_POINTERS.H
 typedef __SIZE_TYPE__ size;
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-#endif /* #ifndef MAIN.C */
+#endif /* #ifndef FUNCTION_POINTERS.H */
