ed5caa617c412a20105e23d5c9fc9a4a3d0205b3
254-elvis <mureithielvis18@gmail.com>
2022-10-13 05:31:51 -0700

    hd

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef MAIN.H
+#define MAIN.H
+typedef __SIZE_TYPE__ size;
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+#endif /* #ifndef MAIN.H */
