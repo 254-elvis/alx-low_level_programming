a189093629a0d374430ff3775fae7a0ea1ec2631
254-elvis <mureithielvis18@gmail.com>
2022-10-24 04:15:55 -0700

    collection

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,49 +1,79 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
-#include <string.h>
 
 /**
- * add_node_end - function that adds a new node at the end of a list_t list
- * @head: input header pointer
- * @str: Input string value
- * Return: the address of the new element, or NULL if it failed
+ * _strlen - calculate the length of a string
+ * @str: the string to calculate the length of
+ *
+ * Return: the length of the string
  */
-
-list_t *add_node_end(list_t **head, const char *str)
+size_t _strlen(const char *str)
 {
-	list_t *node;
-	list_t *tmp;
+	const char *pos = str;
 
-	node = malloc(sizeof(list_t));
-	if (node == NULL)
-		return (NULL);
-	node->len = _strlen(str);
-	node->str = strdup(str);
-	node->next = NULL;
-	if (*head == NULL)
-		*head = node;
-	else
+	if (str)
 	{
-		tmp = *head;
-		while (tmp->next != NULL)
-			tmp = tmp->next;
-		tmp->next = node;
+		while (*pos)
+			++pos;
 	}
-	return (node);
+	return (pos - str);
 }
 
 /**
- * _strlen - returns length of string
- * @s: character of string
- * Return: length of string
+ * _strdup - create a new array containing a copy of the given string
+ * @str: a pointer to the string to copy
+ *
+ * Return: If str is NULL or if memory allocation fails, return NULL.
+ * Otherwise a return a pointer to the new copy
  */
-
-int _strlen(const char *s)
+char *_strdup(const char *str)
 {
-	int i;
+	char *dup = NULL;
+	size_t size = 0;
 
-	while (s[i] != 0)
-		i++;
-	return (i);
+	if (!str)
+		return (NULL);
+
+	while (str[size++])
+		;
+
+	dup = malloc(sizeof(char) * size);
+	if (!dup)
+		return (NULL);
+
+	while (size--)
+		dup[size] = str[size];
+
+	return (dup);
+}
+
+/**
+ * add_node_end - add a string at the end of the list
+ * @head: a pointer to the address of the first list node
+ * @str: the string to add to the list
+ *
+ * Return: If memory allocation fails, return NULL. Otherwise, return the
+ * address of the new no
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new = NULL;
+
+	if (!head)
+		return (NULL);
+
+	if (*head)
+		return (add_node_end(&(*head)->next, str));
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+
+
+	new->str = _strdup(str);
+	new->len = (_strlen(new->str));
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
 }
