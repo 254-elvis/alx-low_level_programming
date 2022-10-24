bb0bc45425285338608e327c1a92e452d91ce4a4
254-elvis <mureithielvis18@gmail.com>
2022-10-24 04:46:32 -0700

    advanced 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 
-#include "lists.h"
+void print_something(void) __attribute__ ((constructor));
 
 /**
- * before_main - funcion that prints a specific string before the main,
- * function in executed.
- *
- * Return: no return.
+ *print_something - prints a msg before the main
  */
 
-
-void before_main(void)
+void print_something(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
 }
